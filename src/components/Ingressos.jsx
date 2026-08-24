c78116e1ecf7e741825d340cbac5001e2ca82d56
Duda Moraes <dudamoraes1402@gmail.com>
2026-08-24 09:39:17 -0300

    feat: codificando ingressos meia e inteira

--- src/components/Ingressos.jsx
+++ src/components/Ingressos.jsx
@@ -1,11 +1,11 @@
 import React, { useState } from 'react'
 
 const filmes =[
-    {id: 1, nome: "Homem-Aranha: Um novo dia ", inteira: 50.00, meia: 25.00, quantidade: 30, disponivel: true},
-    { id: 2, nome: "A odisseia", inteira: 50.00, meia: 25.00, quantidade: 30, disponivel: true },
-    { id: 3, nome: "Sobrenantural: Agora entre nós", inteira: 50.00, meia: 25.00, quantidade: 30, disponivel: true },
-    { id: 4, nome: "o diabo veste prada", inteira: 50.00, meia: 25.00, quantidade: 30, disponivel: false },
-    { id: 5, nome: "Toy Story 5", inteira: 50.00, meia: 25.00, quantidade: 30, disponivel: true }
+    {id: 1, nome: "Homem-Aranha: Um novo dia ", inteira: 50.00, meia: 25.00, quantidadeInteira: 0, quantidadeMeia:0, disponivel: true},
+    { id: 2, nome: "A odisseia", inteira: 50.00, meia: 25.00, quantidadeInteira: 0, quantidadeMeia: 0, disponivel: true },
+    { id: 3, nome: "Sobrenantural: Agora entre nós", inteira: 50.00, meia: 25.00, quantidadeInteira: 0, quantidadeMeia: 0, disponivel: true },
+    { id: 4, nome: "o diabo veste prada", inteira: 50.00, meia: 25.00, quantidadeInteira: 0, quantidadeMeia: 0, disponivel: false },
+    { id: 5, nome: "Toy Story 5", inteira: 50.00, meia: 25.00, quantidadeInteira: 0, quantidadeMeia: 0, disponivel: true }
 ]
 
 const Ingressos = () => {
@@ -13,18 +13,18 @@ const Ingressos = () => {
     const [status, setStatus] = useState("");
     const [enviar, setEnviar] = useState(false);
 
-    const alterarQuantidade = (id, valor) => {
+    const alterarQuantidade = (id, tipo, valor) => {
         setItems(prev => 
             prev.map(item=> 
-                item.id === id ? {...item, quantidade: Math.max(0, item.quantidade + valor )} : item
+                item.id === id ? {...item, [tipo]: Math.max(0, item[tipo] + valor )} : item
             )
         )
     }
 
-    const fimlesDisponiveis = items.filter(item => item.disponivel);
-    const carrinho = items.filter(item => item.quantidade >0);
+    const filmesDisponiveis = items.filter(item => item.disponivel);
+    const carrinho = items.filter(item => item.quantidadeInteira >0 || item.quantidadeMeia >0);
 
-    const subtotal = carrinho.reduce((ac, item) => ac + item.preco * item.quantidade,0);
+    const subtotal = carrinho.reduce((ac, item) => ac + (item.inteira * item.quantidadeInteira) + (item.meia*item.quantidadeMeia),0);
     const total = subtotal > 0 ? subtotal: 0;
 
     const confirmarPedido = () => {
@@ -41,7 +41,7 @@ const Ingressos = () => {
         }
 
 
-        
+
   return (
     <>
       
